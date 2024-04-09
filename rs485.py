57e32b04fc88d76c994cbfb5c2f6b875dbe37a0e
quandinh10 <dinhquan1003@gmail.com>
2024-04-09 17:37:02 +0700

    modify rs485

--- rs485.py
+++ rs485.py
@@ -27,6 +27,20 @@ try:
 except:
     print("Can not open the port")
 
+def serial_read_data(ser):
+    bytesToRead = ser.inWaiting()
+    if bytesToRead > 0:
+        out = ser.read(bytesToRead)
+        data_array = [b for b in out]
+        print(data_array)
+        if len(data_array) >= 7:
+            array_size = len(data_array)
+            value = data_array[array_size - 4] * 256 + data_array[array_size - 3]
+            return value
+        else:
+            return -1
+    return 0
+
 '''
 id , function, data[2:5], crc[6:7]
 mixer 234 (available)
@@ -57,26 +71,12 @@ def setDevice1(state):
     time.sleep(1)
     print(serial_read_data(ser))
 
-# while True:
-#     setDevice1(True)
-#     time.sleep(2)
-#     setDevice1(False)
-#     time.sleep(2)
+while True:
+     setDevice1(True)
+     time.sleep(2)
+     setDevice1(False)
+     time.sleep(2)
 
-
-def serial_read_data(ser):
-    bytesToRead = ser.inWaiting()
-    if bytesToRead > 0:
-        out = ser.read(bytesToRead)
-        data_array = [b for b in out]
-        print(data_array)
-        if len(data_array) >= 7:
-            array_size = len(data_array)
-            value = data_array[array_size - 4] * 256 + data_array[array_size - 3]
-            return value
-        else:
-            return -1
-    return 0
 
 soil_temperature =[1, 3, 0, 6, 0, 1, 100, 11]
 def readTemperature():
