46120b50c93ff4dfd309564f2927c27402a2fb72
quandinh10 <dinhquan1003@gmail.com>
2024-04-18 12:54:54 +0700

    remove ada key

--- adafruit_mqtt.py
+++ adafruit_mqtt.py
@@ -4,8 +4,8 @@ import os
 
 class Adafruit_MQTT:
     AIO_FEED_IDs_sub = ["data"]
-    AIO_USERNAME = "project_IOT_hcmut"
-    AIO_KEY = "aio_Tezi86jSBHFJmRO55MikawC2TNTLabc"
+    AIO_USERNAME = ""
+    AIO_KEY = ""
 
     recvCallBack = None
     client = None
