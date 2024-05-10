bbde62b1e61cd9873f87de99460f7e48cd5497a9
Minh Trí <minhtriinh123@gmail.com>
2024-05-10 14:03:12 +0700

    added parameters to automations

--- android_studio/app/src/main/java/com/iot232/ssis/fragments/AutomationsFragment.java
+++ android_studio/app/src/main/java/com/iot232/ssis/fragments/AutomationsFragment.java
@@ -1,12 +1,18 @@
 package com.iot232.ssis.fragments;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,6 +22,7 @@ import com.iot232.ssis.R;
 import com.iot232.ssis.data.TimerInfo;
 import com.iot232.ssis.databinding.FragmentAutomationsBinding;
 import com.iot232.ssis.recycler.SchedulerAdapter;
+import com.iot232.ssis.recycler.SchedulerViewHolder;
 import com.iot232.ssis.recycler.SchedulerViewInterface;
 
 import java.util.ArrayList;
@@ -28,8 +35,10 @@ public class AutomationsFragment extends Fragment implements SchedulerViewInterf
     RecyclerView schedulerView;
     SchedulerAdapter schedulerAdapter;
     List<TimerInfo> schedules;
-    public int savedPosition = -1;
-    int numUntitled = 0;
+    public int savedpos = -1;
+
+    public final int NAN = 0, MIXER1 = 1, MIXER2 = 2, MIXER3 = 3, PUMP1 = 10,
+            PUMP2 = 11, AREA = 20, NO_TIMER = 30, NO_ACTION = 31, CYCLE = 50, START = 51, TITLE = 52;
 
     private FragmentAutomationsBinding binding;
 
@@ -56,14 +65,13 @@ public class AutomationsFragment extends Fragment implements SchedulerViewInterf
     }
 
     public void addSchedule(){
-        Log.d("POSITION", String.valueOf(savedPosition));
-        int id = (savedPosition != -1)? savedPosition : mainActivity.schedulerInfo.size();
-        savedPosition = -1;
-        String schedulerName = (numUntitled == 0)? "Untitled" : "Untitled " + "(" + String.valueOf(numUntitled) + ")";
-        TimerInfo newSchedule = new TimerInfo(schedulerName, id, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+        Log.d("pos", String.valueOf(savedpos));
+        int id = (savedpos != -1)? savedpos : mainActivity.schedulerInfo.size();
+        savedpos = -1;
+        String schedulerName = "Untitled";
+        TimerInfo newSchedule = new TimerInfo(schedulerName, id, NAN, 0, NAN, NAN, 0, 0, 0, 0, NAN, 0, 0, 0);
         schedules.add(newSchedule);
         mainActivity.schedulerInfo.add(newSchedule);
-        numUntitled++;
         mainActivity.contentHelper.writeContent(mainActivity.schedulerInfo, "schedulerInfo.json", (MainActivity) mainActivity);
         showSchedules();
     }
@@ -86,51 +94,92 @@ public class AutomationsFragment extends Fragment implements SchedulerViewInterf
     }
 
     @Override
-    public void onItemClick(int position) {
-
-    }
-
-    @Override
-    public void onItemDeleted(int position){
-        savedPosition = position;
-        Log.d("POSITION", String.valueOf(savedPosition));
-        schedules.remove(position);
-        mainActivity.schedulerInfo.remove(position);
-        schedulerAdapter.notifyItemRemoved(position);
+    public void onItemClick(int pos) {
+        int inv = 1 - mainActivity.schedulerInfo.get(pos).getSchedulerState();
+        schedules.get(pos).setSchedulerState(inv);
+        mainActivity.schedulerInfo.get(pos).setSchedulerState(inv);
+        if (inv == 1){
+            mainActivity.sendSchedule(pos);
+//            mainActivity.startSchedule(pos, 0, schedulerAdapter);
+        }
+        schedulerAdapter.notifyItemChanged(pos);
         saveSchedules();
     }
 
     @Override
-    public void onInfoChanged(int position, int type, int duration) {
+    public void onItemDeleted(int pos){
+        savedpos = pos;
+        schedules.remove(pos);
+        mainActivity.schedulerInfo.remove(pos);
+        schedulerAdapter.notifyItemRemoved(pos);
+        saveSchedules();
+    }
+
+    @Override
+    public void onInfoChanged(int pos, int type, int duration, long longDuration, String str) {
         switch (type){
-            case 0:
-                schedules.get(position).setMixer1Time(duration);
-                mainActivity.schedulerInfo.get(position).setMixer1Time(duration);
+            case MIXER1:
+                schedules.get(pos).setMixer1Time(duration);
+                mainActivity.schedulerInfo.get(pos).setMixer1Time(duration);
                 break;
-            case 1:
-                schedules.get(position).setMixer2Time(duration);
-                mainActivity.schedulerInfo.get(position).setMixer2Time(duration);
+            case MIXER2:
+                schedules.get(pos).setMixer2Time(duration);
+                mainActivity.schedulerInfo.get(pos).setMixer2Time(duration);
                 break;
-            case 2:
-                schedules.get(position).setMixer3Time(duration);
-                mainActivity.schedulerInfo.get(position).setMixer3Time(duration);
+            case MIXER3:
+                schedules.get(pos).setMixer3Time(duration);
+                mainActivity.schedulerInfo.get(pos).setMixer3Time(duration);
                 break;
-            case 3:
-                schedules.get(position).setPump1Time(duration);
-                mainActivity.schedulerInfo.get(position).setPump1Time(duration);
+            case PUMP1:
+                schedules.get(pos).setPump1Time(duration);
+                mainActivity.schedulerInfo.get(pos).setPump1Time(duration);
                 break;
-            case 4:
-                schedules.get(position).setPump2Time(duration);
-                mainActivity.schedulerInfo.get(position).setPump2Time(duration);
+            case PUMP2:
+                schedules.get(pos).setPump2Time(duration);
+                mainActivity.schedulerInfo.get(pos).setPump2Time(duration);
                 break;
-            case 5:
-                schedules.get(position).setAreaType(duration);
-                mainActivity.schedulerInfo.get(position).setAreaType(duration);
+            case AREA:
+                schedules.get(pos).setAreaType(duration);
+                mainActivity.schedulerInfo.get(pos).setAreaType(duration);
+                break;
+            case CYCLE:
+                schedules.get(pos).setCycleCount(duration);
+                mainActivity.schedulerInfo.get(pos).setCycleCount(duration);
+                break;
+            case START:
+                schedules.get(pos).setMixerStart(longDuration);
+                mainActivity.schedulerInfo.get(pos).setMixerStart(longDuration);
+                break;
+            case TITLE:
+                schedules.get(pos).setSchedulerTitle(str);
+                mainActivity.schedulerInfo.get(pos).setSchedulerTitle(str);
                 break;
             default:
                 break;
         }
-        schedulerAdapter.notifyItemChanged(position);
+        schedulerAdapter.notifyItemChanged(pos);
         saveSchedules();
+    }
+
+    public boolean checkTimer(int pos){
+        int[] time = {mainActivity.schedulerInfo.get(pos).getMixer1Time(),
+                mainActivity.schedulerInfo.get(pos).getMixer2Time(),
+                mainActivity.schedulerInfo.get(pos).getMixer3Time(),
+                mainActivity.schedulerInfo.get(pos).getPump1Time(),
+                mainActivity.schedulerInfo.get(pos).getPump2Time(),
+                mainActivity.schedulerInfo.get(pos).getAreaType(),
+                mainActivity.schedulerInfo.get(pos).getCycleCount()};
+        for (int i : time) {
+            if (i == NAN) return false;
+        }
+        return true;
+    }
+
+    public boolean checkSchedules(int pos){
+        for (int i = 0; i < mainActivity.schedulerInfo.size(); i++){
+            if (i == pos) continue;
+            if (mainActivity.schedulerInfo.get(i).getSchedulerState() == 1) return false;
+        }
+        return true;
     }
 }
